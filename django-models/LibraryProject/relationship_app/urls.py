5d71074bfc3666ac60c72ba2ee915583aa61c78e
Gabriel <yanksongabriel4@gmai.com>
2025-11-08 07:32:13 +0000

    changes made

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,4 @@
-from django.contrib import admin
 from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 
 urlpatterns = [
     path('relationship/', include('relationship_app.urls')),
