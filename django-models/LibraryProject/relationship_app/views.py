941a125922b057cd3c772d365d297a63ec38c067
Gabriel <yanksongabriel4@gmai.com>
2025-11-08 14:26:05 +0000

    new file added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def list_books(request):
     books = Book.objects.all()
@@ -13,3 +15,33 @@ class LibraryDetailView(DetailView):
     context_object_name = "library"
 
 
+# REGISTER VIEW
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+# LOGIN VIEW
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+# LOGOUT VIEW
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
