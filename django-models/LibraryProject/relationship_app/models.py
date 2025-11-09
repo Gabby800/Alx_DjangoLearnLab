89d75eddb84b4170df925ef95b9e2c65052c38cc
Gabriel <yanksongabriel4@gmai.com>
2025-11-09 20:10:57 +0000

    new file added

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -17,6 +17,16 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a new book"),
+            ("can_change_book", "Can change existing book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -51,3 +61,6 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+   
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import admin_view, librarian_view, member_view, register
 from . import views
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),   
@@ -14,6 +15,9 @@ urlpatterns = [
     path("roles/admin/", admin_view, name="admin_view"),
     path("roles/librarian/", librarian_view, name="librarian_view"),
     path("roles/member/", member_view, name="member_view"),
+     path("books/add/", add_book, name="add_book"),
+    path("books/<int:book_id>/edit/", edit_book, name="edit_book"),
+    path("books/<int:book_id>/delete/", delete_book, name="delete_book"),
 ]
 
 
