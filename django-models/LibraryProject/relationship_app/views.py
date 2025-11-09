edd21c9999b7d75720ee4ec5df9ee4cf63164dbf
Gabriel <yanksongabriel4@gmai.com>
2025-11-09 12:54:50 +0000

    changes made

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,8 +44,10 @@ class CustomLogoutView(LogoutView):
 def is_admin(user):
     return hasattr(user, "profile") and user.profile.role == "Admin"
 
+
 def is_librarian(user):
     return hasattr(user, "profile") and user.profile.role == "Librarian"
+
 
 def is_member(user):
     return hasattr(user, "profile") and user.profile.role == "Member"
