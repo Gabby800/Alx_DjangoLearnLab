24695eefe2f38c506ad0dcb5d63007d658795584
Gabriel <yanksongabriel4@gmai.com>
2025-11-11 19:38:18 +0000

    changes made

--- advanced_features_and_security/LibraryProject/relationship_app/apps.py
+++ advanced_features_and_security/LibraryProject/relationship_app/apps.py
@@ -1,14 +1,9 @@
 from django.apps import AppConfig
 
 
-class RelationshipAppConfig(AppConfig):
+class RelationshipApp(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
 
-class RelationshipAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "relationship_app"
-
     def ready(self):
-        # Import signals so receivers get registered at startup
-        from . import signals  # noqa: F401
+        from . import signals 
--- advanced_features_and_security/LibraryProject/relationship_app/signals.py
+++ advanced_features_and_security/LibraryProject/relationship_app/signals.py
@@ -1,15 +1,10 @@
+# yourapp/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.conf import settings
 from .models import UserProfile
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    # Ensure profile saves when user saves
-    if hasattr(instance, "profile"):
-        instance.profile.save()
