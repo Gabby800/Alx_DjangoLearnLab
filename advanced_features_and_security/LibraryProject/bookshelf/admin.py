733994f1b22f1d4b9b3c86b05eb8d7e215db645a
Gabriel <yanksongabriel4@gmai.com>
2025-11-11 20:12:21 +0000

    changes made

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,35 +1,7 @@
 from django.contrib import admin
 from .models import Book
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, UserProfile
+from django.contrib.auth.admin import CustomUserAdmin
+from .models import CustomUser
 
 # Custom admin configuration for the Book model
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    search_fields = ('title', 'author')                     # Search bar fields
-    list_filter = ('publication_year',)                     # Sidebar filter
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
-    )
-    search_fields = ('email',)
-    ordering = ('email',)
-
-# Register your UserProfile
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')
+admin.site.register(CustomUser, CustomUserAdmin)
