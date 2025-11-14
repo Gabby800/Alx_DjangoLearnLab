eae258d25c63d91702e7883cbc369334032c1ae2
Gabriel <yanksongabriel4@gmai.com>
2025-11-14 08:39:40 +0000

    changes made

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,13 +1,5 @@
 from django import forms
-from .models import Book
 
-class Exampleform(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
-    # Additional validation example
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if '<script>' in title.lower():
-            raise forms.ValidationError("Invalid characters in title.")
-        return title
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
