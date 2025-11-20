2da1df243acccda4aa40a375ecaa7ded9029e18c
Gabriel <yanksongabriel4@gmai.com>
2025-11-20 09:58:41 +0000

    newfile added

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    published_year = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
