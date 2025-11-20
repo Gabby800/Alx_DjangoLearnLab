07fee9857eeef477eab7b611f933c838014d2361
Gabriel <yanksongabriel4@gmai.com>
2025-11-20 10:13:37 +0000

    changes made

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from .models import Book
 from .serializers import BookSeriaizer
+from .models import Book
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
