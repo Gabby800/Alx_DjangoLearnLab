4ecdfcb09bc15b5d4708c0231fa1d56c0eaa782d
Gabriel <yanksongabriel4@gmai.com>
2025-11-21 20:29:25 +0000

    changes ,made to Bookauth

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -118,6 +118,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# settings.py
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
