f3f4b43edeb3cf25be98cf3ed11682925f3e6542
Gabriel <yanksongabriel4@gmai.com>
2025-11-27 18:55:18 +0000

    Views updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save(last_updated_by=self.request.user)
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny] 
+
+    def perform_create(self, serializer):
+        print("Creating new book:", serializer.validated_data)
+        serializer.save()
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def perform_update(self, serializer):
+        print("Updating book:", serializer.validated_data)
+
+        publication_year = serializer.validated_data.get("publication_year")
+        if publication_year:
+            from datetime import datetime
+            current_year = datetime.now().year
+            if publication_year > current_year:
+                raise ValueError("Publication year cannot be in the future.")
+
+        serializer.save()
