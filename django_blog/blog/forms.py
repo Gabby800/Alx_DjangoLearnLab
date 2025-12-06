2061c9f15060a8c2a2c60461bc292085eea5432b
Gabriel <yanksongabriel4@gmai.com>
2025-12-06 12:51:22 +0000

    changes made

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
-from taggit.forms import TagWidget  # <-- import TagWidget
+from taggit.forms import TagWidget  
 
 class CustomUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
