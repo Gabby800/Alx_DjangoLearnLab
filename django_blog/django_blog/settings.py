179a5089f76ad3c199927d479d020fb7fa7898fa
Gabriel <yanksongabriel4@gmai.com>
2025-12-06 12:37:34 +0000

    changes made

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'taggit',
+    "taggit",
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
