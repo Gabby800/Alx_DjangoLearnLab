c0154591682f2d0c2978e439e6cb73d9ed63f297
Gabriel <yanksongabriel4@gmai.com>
2025-12-11 18:51:20 +0000

    changes made to like and unlike URLs

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,6 +9,6 @@ router.register(r'comments', CommentViewSet, basename='comments')
 urlpatterns = [
     path('', include(router.urls)),
     path("feed/", FeedView.as_view(), name='user-feed'),
-    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
